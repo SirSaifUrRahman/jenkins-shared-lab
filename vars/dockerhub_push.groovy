dc3918178c2ffd4712f5509ea0964402b650cc84
saif_ur_rahman <166166418+SirSaifUrRahman@users.noreply.github.com>
2025-02-02 16:36:51 +0500

    Update dockerhub_push.groovy

--- vars/dockerhub_push.groovy
+++ vars/dockerhub_push.groovy
@@ -1,4 +1,4 @@
-def call(String credentialsId, String imageName, String imageTag) {
+def call(String credentialsId, String dockerHubUser, String imageName, String imageTag) {
     withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
         sh """
             echo "\$DOCKER_PASS" | docker login -u "\$DOCKER_USER" --password-stdin
